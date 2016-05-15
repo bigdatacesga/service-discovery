128a3d334b7a41319117ecaf23bfaeba0a270159
Javier Cacheiro <jlopez@cesga.es>
2016-05-16 00:12:50 +0200

    0.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='consul-service-discovery',
-    version='0.1.1',
+    version='0.1.2',
     author='Javier Cacheiro',
     author_email='javier.cacheiro@gmail.com',
     url='https://github.com/javicacheiro/consul-service-discovery',
